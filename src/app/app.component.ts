96eb020f572d2f259b99c5c49708191513f1cd78
jason zheng <tso1158687@gmail.com>
2024-03-17 21:21:37 +0800

    add ngx-lottie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ButtonModule],
+  imports: [RouterOutlet,ButtonModule,LottieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  options: AnimationOptions = {
+    path: '/assets/lottie/eat.json',
+  };
   title = 'what-to-eat-in-list';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),provideLottieOptions({
+    player: () => player,
+  }),]
 };
